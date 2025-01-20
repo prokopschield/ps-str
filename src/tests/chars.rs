b23c290225f93a2e1d7dca3f2d50e0fece84fd50
Prokop <prokop.schield@protonmail.com>
2025-01-20 23:33:38 +0100

    feat: add pretty character table

---
+++ src/tests/chars.rs
@@ -0,0 +1,125 @@
+use std::{fs::OpenOptions, io::Write};
+
+use encoding::{
+    all::{ISO_8859_1, ISO_8859_2, WINDOWS_1250, WINDOWS_1252},
+    Encoding,
+};
+
+use crate::Utf8Encoder;
+
+#[test]
+pub fn char_table() -> Result<(), std::io::Error> {
+    let mut file = OpenOptions::new().create(true).write(true).open("chars")?;
+
+    writeln!(file, "0x00 CAN LA1 LA2 W52 W50 L1 L2 W2 W0")?;
+
+    let fmt = |str: &str| {
+        let fmt = format!("{:?}", str);
+
+        if fmt.chars().count() > 3 {
+            let chars: Vec<char> = str.chars().collect();
+
+            format!("{}", chars[0] as u8)
+        } else {
+            fmt
+        }
+    };
+
+    let checkmark = |condition: bool| {
+        if condition {
+            "✅"
+        } else {
+            "❌"
+        }
+    };
+
+    let invalid = |s: &str| {
+        let chars: Vec<char> = s.chars().collect();
+        let c = chars[0];
+
+        let valid = ['\\', '\"', '', ' ', '­'];
+
+        c >= ' ' && !valid.contains(&c) && format!("{s:?}").chars().count() > 3
+    };
+
+    for i in u8::MIN..=u8::MAX {
+        let raw = [i];
+
+        let canonical = raw.to_utf8_string();
+
+        let latin1 = ISO_8859_1
+            .decode(&raw, encoding::DecoderTrap::Replace)
+            .expect("Failed decoding ISO_8859_1");
+
+        let latin2 = ISO_8859_2
+            .decode(&raw, encoding::DecoderTrap::Replace)
+            .expect("Failed decoding ISO_8859_2");
+
+        let win1252 = WINDOWS_1252
+            .decode(&raw, encoding::DecoderTrap::Replace)
+            .expect("Failed decoding WINDOWS_1252");
+
+        let win1250 = WINDOWS_1250
+            .decode(&raw, encoding::DecoderTrap::Replace)
+            .expect("Failed decoding WINDOWS_1250");
+
+        if !invalid(&canonical)
+            && canonical == latin1
+            && canonical == latin2
+            && canonical == win1252
+            && canonical == win1250
+        {
+            continue;
+        }
+
+        writeln!(
+            file,
+            "0x{:2x} {} {} {} {} {} {} {} {} {}",
+            i,
+            fmt(&canonical),
+            fmt(&latin1),
+            fmt(&latin2),
+            fmt(&win1252),
+            fmt(&win1250),
+            checkmark(canonical == latin1),
+            checkmark(canonical == latin2),
+            checkmark(canonical == win1252),
+            checkmark(canonical == win1250)
+        )?;
+
+        assert!(
+            !invalid(&canonical),
+            "Disallowed character {} '{}'",
+            fmt(&canonical),
+            canonical
+        );
+
+        if canonical == latin2 {
+            continue;
+        } else {
+            assert!(invalid(&latin2), "{i} should be {latin2}, is {canonical}");
+        }
+
+        if canonical == win1250 {
+            continue;
+        } else {
+            assert!(invalid(&win1250), "{i} should be {win1250}, is {canonical}");
+        }
+
+        if canonical == win1252 {
+            continue;
+        } else {
+            assert!(invalid(&win1252), "{i} should be {win1252}, is {canonical}");
+        }
+
+        if canonical == latin1 {
+            continue;
+        } else {
+            assert!(invalid(&latin1), "{i} should be {latin1}, is {canonical}");
+        };
+
+        assert!(canonical == "�", "Character {canonical} not match encoding");
+    }
+
+    Ok(())
+}
--- src/tests/latin2.rs
+++ src/tests/latin2.rs
@@ -1,10 +1,34 @@
+use encoding::Encoding;
 
 use crate::*;
 
 #[test]
 pub fn zlutoucky_kun() {
-    let bytes = b"P\xc5\x99\xc3\xadli\xc5\xa1 \xc5\xbelu\xc5\xa5ou\xc4\x8dk\xc3\xbd k\xc5\xaf\xc5\x88 \xc3\xbap\xc4\x9bl \xc4\x8f\xc3\xa1belsk\xc3\xa9 \xc3\xb3dy.";
     let str = "Příliš žluťoučký kůň úpěl ďábelské ódy.";
 
-    assert_eq!(bytes.to_utf8_string(), str, "Decoding failed");
+    let utf8 = b"P\xc5\x99\xc3\xadli\xc5\xa1 \xc5\xbelu\xc5\xa5ou\xc4\x8dk\xc3\xbd k\xc5\xaf\xc5\x88 \xc3\xbap\xc4\x9bl \xc4\x8f\xc3\xa1belsk\xc3\xa9 \xc3\xb3dy.";
+
+    assert_eq!(str.as_bytes(), utf8, "utf8 does not match str");
+
+    let latin2 = encoding::all::ISO_8859_2
+        .encode(str, encoding::EncoderTrap::Strict)
+        .expect("Failed to encode str as latin2");
+
+    assert_eq!(
+        latin2.to_utf8_string(),
+        utf8.to_utf8_string(),
+        "latin2 does not match utf8"
+    );
+
+    let win1250 = encoding::all::WINDOWS_1250
+        .encode(str, encoding::EncoderTrap::Strict)
+        .expect("Failed to encode str as win1250");
+
+    assert_ne!(latin2, win1250, "latin2 should not match win1250");
+
+    assert_eq!(
+        latin2.to_utf8_string(),
+        win1250.to_utf8_string(),
+        "latin2 does not match win1250"
+    );
 }
